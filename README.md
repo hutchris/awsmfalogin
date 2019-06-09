1953f29993a2446a018c06109bf9f90bd3b34a14
ch663p <chris.hutchings@intl.att.com>
2019-06-09 14:20:13 +1200

    Added username argument to avoid having to find mfa serial number

--- README.md
+++ README.md
@@ -4,14 +4,13 @@ Speeds up the process of getting an AWS session token into your environment vari
 Add your IAM key using the `aws configure` command
 
 run:  
-`python3 awsmfalogin.py -s <your mfa serial number> <your mfa token>`
+`python3 awsmfalogin.py -u <your aws username> <your mfa token>`
+
+You can also specify your mfa device serial number:
+`python3 awsmfalogin.py -s <your aws mfa device serial number> <your mfa token>`
 
 This will print out the commands you need to run to get the new IAM creds into environment variables. Copy-paste them into the terminal, if you are on linux you can execute the output automatically by running as a subcommand:  
-`$(python3 awsmfalogin.py -s <your mfa serial number> <your mfa token>)`
+`$(python3 awsmfalogin.py -u <your aws username> <your mfa token>)`
 
 You need boto3 installed for this to work. This script will always use the IAM creds in your .aws/credentials file
 
-You can prevent the need to type in your serial number every time by storing it as a default in the argparse argument. Change this:  
-`parser.add_argument('--serialnumber','-s',help='serial number from mfa token, found in your IAM user profile in the AWS console')`  
-To this:  
-`parser.add_argument('--serialnumber','-s',help='serial number from mfa token, found in your IAM user profile in the AWS console',default='arn:aws:iam::0123012301230123:mfa/example')`  
--- awsmfalogin.py
+++ awsmfalogin.py
@@ -3,14 +3,31 @@ import platform
 import boto3
 import argparse
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(
+	description='Speeds up the process of getting an AWS session token into your environment variables',
+	epilog='You must enter either --username/-u or --serialnumber/-s'
+	)
 parser.add_argument('token',help='Value from the authenticator token on your phone')
+parser.add_argument('--username','-u',help='AWS username')
 parser.add_argument('--serialnumber','-s',help='serial number from mfa token, found in your IAM user profile in the AWS console')
 args = parser.parse_args()
 
+if args.serialnumber is None and args.username is None:
+	raise(Error("Either username or serialnumber must be specified"))
+
 session = boto3.Session(profile_name='default')
+
+if args.serialnumber is None:
+	iam = session.client('iam')
+	mfaDevices = iam.list_virtual_mfa_devices()
+	for device in mfaDevices['VirtualMFADevices']:
+		 if device['User']['UserName'] == args.username:
+		 	serialnumber = device['SerialNumber']
+else:
+	serialnumber = args.serialnumber
+
 sts = session.client('sts')
-creds = sts.get_session_token(SerialNumber=args.serialnumber,TokenCode=args.token)
+creds = sts.get_session_token(SerialNumber=serialnumber,TokenCode=args.token)
 
 ak = 'AWS_ACCESS_KEY_ID={}'.format(creds['Credentials']['AccessKeyId'])
 sk = 'AWS_SECRET_ACCESS_KEY={}'.format(creds['Credentials']['SecretAccessKey'])
